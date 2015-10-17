5c014ad4953aa013222788a839b78e90b834638a
nogic <136042224@qq.com>
2015-10-18 01:25:29 +0800

    添加本地服务模型

---
+++ putaolife/putaolife/Life/Model/PTNactivity.h
@@ -0,0 +1,13 @@
+//
+//  PTNactivity.h
+//  putaolife
+//
+//  Created by 玉良彭 on 15/10/18.
+//  Copyright © 2015年 pengyl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTNactivity : NSObject
+
+@end
---
+++ putaolife/putaolife/Life/Model/PTNactivity.m
@@ -0,0 +1,13 @@
+//
+//  PTNactivity.m
+//  putaolife
+//
+//  Created by 玉良彭 on 15/10/18.
+//  Copyright © 2015年 pengyl. All rights reserved.
+//
+
+#import "PTNactivity.h"
+
+@implementation PTNactivity
+
+@end
